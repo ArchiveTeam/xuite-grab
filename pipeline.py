c969e317506661923760b92baff330d8408172e8
arkiver <arkiver@protonmail.com>
2023-08-20 19:11:12 +0200

    Version 20230820.04. Multi item size 100.

--- pipeline.py
+++ pipeline.py
@@ -125,11 +125,11 @@ if not WGET_AT:
 #
 # Update this each time you make a non-cosmetic change.
 # It will be added to the WARC files and reported to the tracker.
-VERSION = '20230820.03'
+VERSION = '20230820.04'
 USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'
 TRACKER_ID = 'xuite'
 TRACKER_HOST = 'legacy-api.arpa.li'
-MULTI_ITEM_SIZE = 1
+MULTI_ITEM_SIZE = 100
 
 
 ###########################################################################
